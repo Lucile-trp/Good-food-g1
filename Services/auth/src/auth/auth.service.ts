cfb30a280b3264d3a17e0299276be8986efecb08
Lucile TRIPIER <tripier.lucile@gmail.com>
2023-12-15 16:33:27 +0100

    feat(auth) : signIn fnc with JWT

--- Services/auth/src/auth/auth.service.ts
+++ Services/auth/src/auth/auth.service.ts
@@ -1,19 +1,31 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../users/users.service';
 import { hashPassword } from 'src/helpers/hashPassword';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UserService) {}
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async signIn(_email: string, _password: string): Promise<string> {
+  async signIn(
+    _email: string,
+    _password: string,
+  ): Promise<{ access_token: string }> {
     const user = await this.usersService.getUserByEmail(_email);
     if (user?.password !== (await hashPassword(_password))) {
       throw new UnauthorizedException();
     }
-    const { email } = user;
-    // TODO: Generate a JWT and return it here
-    // instead of the user object
-    return email;
+    // JWT generation & return
+    const payload = { sub: user._id };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+
+  async signUp(_email: string, _password: string) {
+    console.log(_email, _password);
   }
 }
