f7a8548e81044fc85c511e86936dad4c95a1a927
Lucile-trp <49020594+Lucile-trp@users.noreply.github.com>
2024-11-04 16:47:17 +0100

    Feat/auth/jwt implementation (#30)
    * feat(auth): add logIn with JWT generation
    
    * feat(auth): add TCP microservice connection
    
    * fix(): prettier errors
    
    * fix(npm) : libs vulnerabilities
    
    * ref(app): reload app and add .env exemple
    
    * feat(auth) : user login with JWT return
    
    * feat(auth) : add try catch & user feedbacks & AuthResponse Pattern
    
    * feat(auth): add register user
    
    * fix(auth) : fix discard file
    
    * fix(auth) : delete discard function

--- Services/auth/src/auth/auth.module.ts
+++ Services/auth/src/auth/auth.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
-
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'), // Utilisation de ConfigService
+        signOptions: { expiresIn: '8h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
