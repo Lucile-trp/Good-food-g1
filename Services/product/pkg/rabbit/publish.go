6db36bb35de02137338b87c64f54c2010552e389
egamorf76 <maxime.adler76@gmail.com>
2024-11-16 23:19:02 +0100

    fix warning

--- Services/product/pkg/rabbit/publish.go
+++ Services/product/pkg/rabbit/publish.go
@@ -42,7 +42,8 @@ func (r Rabbit) PublishDish(l logger.Interface, p *repo.ProductRepo, order rpcEn
 
 	body := string(data)
 
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
 	err = r.ch.PublishWithContext(ctx,
 		"",
