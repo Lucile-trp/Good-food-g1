2e1c97e369ffeefa2748910612c88aa22c03268e
Lucile TRIPIER <tripier.lucile@gmail.com>
2024-11-16 12:41:40 +0100

    feat(home & user) : add profil page & add home page informations

--- Apps/web/app/page.tsx
+++ Apps/web/app/page.tsx
@@ -1,15 +1,70 @@
-import Header from '@/components/UI/Header'
-
 export default function Home() {
   return (
-    <main className="grow bg-white">
-      <Header isConnected={true} ></Header>
-      <div className='bg-stone'>
-        <div className=''></div>
-        <h1 className="text-black">BIENVENUE SUR GOODFOOD</h1>
-      </div>
-    
-       
+    <main className="grow bg-background flex flex-col items-center justify-center">
+      
+      {/* HOME SECTION */}
+      <section>
+      <div className="absolute left-0 bg-[#D9D9D9] h-64 w-full"></div>
+        
+        {/* GRID */}
+        <div className="grid grid-cols-4 gap-4 mt-6">
+          <div className="col-span-4 lg:col-span-4 lg:col-start-2 relative">
+            <img
+              src="/images/img1.jpg"
+              alt="Plat en vedette"
+              className="w-full h-auto object-cover rounded shadow-lg border border-gray-300"
+            />
+          </div>
+        </div>
+        {/* ABSOLUTE */}
+        <div className="absolute top-12 bg-opacity-80 h-full content-center">
+          <h1 className="text-5xl font-extrabold text-black leading-tight text-stroke-white">
+            BIENVENUE SUR GOODFOOD
+          </h1>
+          <div className="mt-6 flex flex-col gap-2">
+            <label
+              htmlFor="restaurant"
+              className="block text-lg font-medium mb-2 text-black"
+            >
+              Choisissez votre restaurant
+            </label>
+            <input
+              id="restaurant"
+              type="text"
+              placeholder="Restaurants"
+              className="w-full px-4 py-2 border border-dark_gray rounded focus:outline-none"
+            />
+            <button className="px-6 py-2 bg-black text-white rounded w-32">
+              Accéder
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <div className="h-px w-[75%] bg-dark_gray mt-10"/>
+
+      {/* DELIVERY SECTION */}
+      <section className="">
+        <div className="my-10 mx-auto flex gap-8">
+          <div className="flex-1">
+            <img
+              src="/images/img2.jpg"
+              alt="Livreur"
+              className="w-full h-auto rounded shadow"
+            />
+          </div>
+          <div className="flex-1">
+            <h2 className="text-5xl font-bold mb-4 text-black">
+              Livrez avec nous !
+            </h2>
+            <p className="text-black">
+              But I must explain to you how all this mistaken idea of denouncing
+              pleasure...
+            </p>
+          </div>
+        </div>
+      </section>
+
     </main>
-  )
+  );
 }
--- Apps/web/components/UI/Header.tsx
+++ Apps/web/components/UI/Header.tsx
@@ -9,25 +9,25 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ isConnected }) => {
   return (
-    <header className="flex items-center justify-between  bg-white border border-gray-200 md:px-8">
-      <h1 className="text-2xl font-bold text-black">GOODFOOD</h1>
+    <header className="flex items-center justify-between bg-white border-b border-dark_gray md:px-8 xl:px-64">
+      <Image src="/logo/png/black/logo_black.png" height={50} width={180} alt="Logo Goodfood"></Image>
 
       <nav className="flex items-center">
         {!isConnected ? (
           <div className="pt-4">
-            <button className="px-4 py-2 text-white bg-black border-r hover:bg-gray-800">
+            <button className="px-4 py-2 text-white bg-black border-r hover:bg-secondary_purple">
               Se connecter
             </button>
-            <button className="px-4 py-2 text-white bg-black hover:bg-gray-800">
+            <button className="px-4 py-2 text-white bg-black hover:bg-secondary_green">
               S'inscrire
             </button>
           </div>
         ) : (
           <div className="flex pt-4 items-end">
-            <button className="px-4 py-2 text-white bg-black border-r hover:bg-gray-800 h-10">
+            <button className="px-4 py-2 text-white bg-black border-r hover:bg-secondary_green h-10">
               Panier
             </button>
-            <button className="px-4 py-2 text-white bg-black hover:bg-gray-800 h-10">
+            <button className="px-4 py-2 text-white bg-black hover:bg-secondary_purple h-10">
               <Image
                 src="/icons/png/white/user_icon.png"
                 width={20}
