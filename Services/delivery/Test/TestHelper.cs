dfa4830526b1db2be0ae7777d3ff6653ffad2354
egamorf76 <maxime.adler76@gmail.com>
2024-11-18 22:55:13 +0100

    add dish service unit test

---
+++ Services/delivery/Test/TestHelper.cs
@@ -0,0 +1,36 @@
+﻿using Host.Data;
+using Host.Interfaces.Services;
+using Host.Repository;
+using Host.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class TestHelper
+    {
+        private readonly DeliveryDbContext deliveryDbContext;
+
+        public TestHelper()
+        {
+            var builder = new DbContextOptionsBuilder<DeliveryDbContext>();
+            builder.UseInMemoryDatabase(databaseName: "DeliveryDbInMemory");
+
+            var dbContextOptions = builder.Options;
+            deliveryDbContext = new DeliveryDbContext(dbContextOptions);
+            // Delete existing db before creating a new one
+            deliveryDbContext.Database.EnsureDeleted();
+            deliveryDbContext.Database.EnsureCreated();
+        }
+
+        public IDishService GetInMemoryDishService()
+        {
+            var repo = new DishRepository(deliveryDbContext);
+            return new DishService(repo);
+        }
+    }
+}
